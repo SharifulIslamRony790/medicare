7fa2c6c9bd67e9f68d6c68f58e1d2f90d9b463b7
SharifulIslamRony790 <mdsharifulislamrony790@gmail.com>
2026-09-18 17:25:08 +0600

    chore: force debug true to see exact error

--- medicare_core/settings.py
+++ medicare_core/settings.py
@@ -28,7 +28,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
